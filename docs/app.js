e7c476e5db20d040c5dce0de85065dafe4e16435
刘胜 <wb-ls269090@alibaba-inc.com>
2018-10-23 15:39:25 +0800

    update build

--- docs/app.js
+++ docs/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Markdown from 'react-markdown';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import 'babel-polyfill';
 import Layout from './components/Layout';
 import CodeBlock from './code-block';
