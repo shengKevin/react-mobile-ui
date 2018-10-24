b45b3a26d4addf6d8813e0ff7be0ca8387098798
刘胜 <wb-ls269090@alibaba-inc.com>
2018-10-24 15:50:41 +0800

    docs layout

--- docs/app.js
+++ docs/app.js
@@ -22,9 +22,34 @@ import {
   IconDocs,
 } from '../src/md.js';
 
-// const btnMark = require('../src/components/Button/index.md');
-
-const ReactMarkdown = (md) => { return () => <Layout routers={routers}><Markdown source={md} renderers={{code: CodeBlock}} /></Layout>; };
+const ReactMarkdown = (md) => {
+  if (!md) return null;
+  const mdTitle = md.match(/##\s\S*/g);
+  const str = mdTitle[0] || '';
+  const title = str.replace('##', '').trim();
+  const source = `https://shengkevin.github.io/react-mui/example/index.html#/${title}`;
+  const introduce = md.split('## code')[0];
+  const api = md.match(/## API[\s\S]*/)[0];
+  const code = md.replace(api, '').replace(introduce, '').replace('## code', '');
+  return () => (
+    <Layout routers={routers}>
+      <div className="layout-header">
+        <Markdown source={introduce} renderers={{code: CodeBlock}} />
+        <p style={{ marginTop: 12}}>如有滚动效果，请用手机模式点击<a style={{color: '#1890ff'}} href={source}>这里</a></p>
+      </div>
+      <div className="layout-content">
+        <div className="layout-mobile">
+          <iframe 
+            title={title} 
+            src={source}
+            style={{ width: 375, height: 667, border: '1px solid rgb(247,247,247)' }}
+          />
+        </div>
+        <div className="layout-content-code"><Markdown source={code} renderers={{code: CodeBlock}} /></div>
+      </div>
+      <div className="layout-footer"><Markdown source={api} renderers={{code: CodeBlock}} /></div>
+    </Layout>);
+};
 
 const routers = [
   {path: '/start', component: ReactMarkdown(''), exact: true},
@@ -62,7 +87,6 @@ class App extends React.Component {
             <Redirect to="/start" />
           </Switch>
         </Router>
-        {/* <iframe src="https://shengkevin.github.io/button"></iframe> */}
       </div>
     );
   }
